fcc04d87e4ee6a319b1896065172b10e5cf5b8a6
Gabriel <gabriel_schaedler@hotmail.com>
2024-10-10 21:19:00 -0300

    Updateresolvendocagada

--- src/components/Mapa.jsx
+++ src/components/Mapa.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback, useEffect } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { GoogleMap, LoadScript, DirectionsRenderer, Marker } from '@react-google-maps/api';
 import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
@@ -9,22 +9,23 @@ const containerStyle = {
 };
 
 const initialCenter = {
-  lat: -25.5469,
-  lng: -54.5882,
+  lat: -25.5469,  // Latitude of Foz do Iguaçu, Brazil
+  lng: -54.5882   // Longitude of Foz do Iguaçu, Brazil
 };
+
+// Use the API key from the .env file
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const Mapa = () => {
   const [zoom, setZoom] = useState(14);
   const [center, setCenter] = useState(initialCenter);
   const mapRef = useRef(null);
-  const [addresses, setAddresses] = useState(['', '', '']);
+
+  const [addresses, setAddresses] = useState(['', '', '']); // Three default empty addresses
   const [directions, setDirections] = useState(null);
   const [waypointsMarkers, setWaypointsMarkers] = useState([]);
   const [totalDistance, setTotalDistance] = useState(null);
   const [googleMapsUrl, setGoogleMapsUrl] = useState('');
-
-  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
 
   const onLoad = useCallback((map) => {
     mapRef.current = map;
@@ -34,14 +35,17 @@ const Mapa = () => {
     mapRef.current = null;
   }, []);
 
+  // Function to use current location
   const handleUseCurrentLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
         const currentLocation = `${latitude}, ${longitude}`;
+
         const updatedAddresses = [...addresses];
         updatedAddresses[0] = currentLocation;
         setAddresses(updatedAddresses);
+
         if (mapRef.current) {
           mapRef.current.panTo({ lat: latitude, lng: longitude });
           mapRef.current.setZoom(14);
@@ -55,94 +59,185 @@ const Mapa = () => {
     }
   };
 
+  const handleOptimizeRoute = () => {
+    const addressesArray = addresses.filter(address => address.trim() !== '');
+
+    if (addressesArray.length < 2) {
+      alert('Por favor, insira pelo menos dois endereços.');
+      return;
+    }
+
+    const origin = addressesArray[0];
+    const waypoints = addressesArray.slice(1, -1);
+
+    const distanceMatrixService = new window.google.maps.DistanceMatrixService();
+
+    distanceMatrixService.getDistanceMatrix(
+      {
+        origins: addressesArray,
+        destinations: addressesArray,
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      },
+      (response, status) => {
+        if (status === 'OK') {
+          try {
+            const distanceData = response.rows;
+
+            if (!distanceData || distanceData.length === 0 || !distanceData[0].elements) {
+              throw new Error("Incomplete response from Distance Matrix API");
+            }
+
+            const optimizedOrder = getOptimalOrder(distanceData);
+            const reorderedWaypoints = optimizedOrder.slice(1).map(index => ({
+              location: addressesArray[index],
+            }));
+
+            const directionsService = new window.google.maps.DirectionsService();
+            directionsService.route(
+              {
+                origin: addressesArray[optimizedOrder[0]],
+                destination: addressesArray[optimizedOrder[optimizedOrder.length - 1]],
+                waypoints: reorderedWaypoints,
+                travelMode: window.google.maps.TravelMode.DRIVING
+              },
+              (result, status) => {
+                if (status === window.google.maps.DirectionsStatus.OK) {
+                  setDirections(result);
+
+                  const markers = [];
+
+                  markers.push({
+                    location: result.routes[0].legs[0].start_location,
+                    label: {
+                      text: '1',
+                      color: 'white',
+                      fontWeight: 'bold'
+                    },
+                    key: 'wp-origin'
+                  });
+
+                  result.routes[0].legs.slice(0, -1).forEach((leg, i) => {
+                    markers.push({
+                      location: leg.end_location,
+                      label: {
+                        text: (i + 2).toString(),
+                        color: 'white',
+                        fontWeight: 'bold'
+                      },
+                      key: `wp-${i}`
+                    });
+                  });
+
+                  setWaypointsMarkers(markers);
+
+                  let totalDistance = 0;
+                  result.routes[0].legs.forEach(leg => {
+                    totalDistance += leg.distance.value;
+                  });
+                  totalDistance /= 1000; // Convert to km
+                  setTotalDistance(totalDistance);
+
+                  // Construct Google Maps URL
+                  const destination = addressesArray[optimizedOrder[optimizedOrder.length - 1]];
+                  const waypointsForUrl = optimizedOrder.slice(1, -1).map(index => addressesArray[index]).join('|');
+                  const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&origin=${encodeURIComponent(origin)}&destination=${encodeURIComponent(destination)}&waypoints=${encodeURIComponent(waypointsForUrl)}`;
+
+                  setGoogleMapsUrl(googleMapsUrl);
+                } else {
+                  console.error(`Error fetching directions: ${status}`);
+                  alert('Houve um erro ao obter a rota. Por favor, tente novamente.');
+                }
+              }
+            );
+          } catch (error) {
+            console.error('Error processing Distance Matrix response:', error);
+            alert('Um ou mais dos endereços possui pouca informação. Adicione, por exemplo, a cidade, nome da rua ou número.');
+          }
+        } else {
+          console.error(`Distance Matrix request failed: ${status}`);
+          alert('Falha ao calcular as distâncias. Verifique se todos os endereços estão corretos.');
+        }
+      }
+    );
+  };
+
+  const getOptimalOrder = (distanceData) => {
+    const n = distanceData.length;
+    const visited = new Array(n).fill(false);
+    const order = [];
+    let currentIndex = 0;
+    visited[0] = true;
+    order.push(0);
+
+    for (let i = 1; i < n; i++) {
+      let nextIndex = -1;
+      let minDistance = Number.MAX_SAFE_INTEGER;
+
+      for (let j = 0; j < n; j++) {
+        if (!visited[j] && distanceData[currentIndex].elements[j].distance.value < minDistance) {
+          minDistance = distanceData[currentIndex].elements[j].distance.value;
+          nextIndex = j;
+        }
+      }
+
+      visited[nextIndex] = true;
+      order.push(nextIndex);
+      currentIndex = nextIndex;
+    }
+
+    return order;
+  };
+
   const handleAddressChange = (index, value) => {
     const updatedAddresses = [...addresses];
     updatedAddresses[index] = value;
     setAddresses(updatedAddresses);
   };
 
-  const handleOptimizeRoute = () => {
-    const waypoints = addresses.slice(1, addresses.length - 1).filter((addr) => addr !== '');
-    if (addresses[0] && addresses[addresses.length - 1]) {
-      const directionsService = new window.google.maps.DirectionsService();
-      const request = {
-        origin: addresses[0],
-        destination: addresses[addresses.length - 1],
-        waypoints: waypoints.map((waypoint) => ({ location: waypoint, stopover: true })),
-        travelMode: window.google.maps.TravelMode.DRIVING,
-        optimizeWaypoints: true,
-      };
-
-      directionsService.route(request, (result, status) => {
-        if (status === window.google.maps.DirectionsStatus.OK) {
-          setDirections(result);
-
-          const legs = result.routes[0].legs;
-          const distanceSum = legs.reduce((sum, leg) => sum + leg.distance.value, 0);
-          setTotalDistance(distanceSum / 1000); // Convert to km
-
-          setWaypointsMarkers([
-            { location: result.routes[0].legs[0].start_location, label: { text: 'Start' }, key: 'start' },
-            ...legs.map((leg, index) => ({
-              location: leg.end_location,
-              label: { text: `Stop ${index + 1}` },
-              key: `stop_${index + 1}`,
-            })),
-          ]);
-
-          const url = `https://www.google.com/maps/dir/?api=1&origin=${encodeURIComponent(addresses[0])}&destination=${encodeURIComponent(addresses[addresses.length - 1])}&travelmode=driving&waypoints=${waypoints.map((wp) => encodeURIComponent(wp)).join('|')}`;
-          setGoogleMapsUrl(url);
-        } else {
-          console.error('Error fetching directions:', result);
-        }
-      });
-    } else {
-      alert('Please fill in both the start and end addresses.');
-    }
+  const handleAddAddress = () => {
+    setAddresses([...addresses, '']);
   };
 
   return (
     <div className="flex flex-col md:flex-row md:h-[85vh] md:ml-[25%]">
       <div className="md:w-1/2 p-4 ">
         <div className="bg-gray-800 h-[400px] md:h-full">
-          {apiKey && (
-            <LoadScript googleMapsApiKey={apiKey}>
-              <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={zoom}
-                onLoad={onLoad}
-                onUnmount={onUnmount}
-                options={{
-                  streetViewControl: false,
-                  disableDefaultUI: true,
-                  zoomControl: true,
-                  tilt: 0,
-                  rotateControl: false,
-                  mapTypeControl: false,
-                  mapTypeId: "satellite"
-                }}
-              >
-                {directions && (
-                  <DirectionsRenderer
-                    directions={directions}
-                    options={{ suppressMarkers: true }}
-                  />
-                )}
-                {waypointsMarkers.map((marker) => (
-                  <Marker
-                    key={marker.key}
-                    position={marker.location}
-                    label={{
-                      text: marker.label.text,
-                      color: 'white',
-                      fontWeight: 'bold'
-                    }}
-                  />
-                ))}
-              </GoogleMap>
-            </LoadScript>
-          )}
+          <LoadScript googleMapsApiKey={apiKey}>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={zoom}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
+              options={{
+                streetViewControl: false,
+                disableDefaultUI: true,
+                zoomControl: true,
+                tilt: 0,
+                rotateControl: false,
+                mapTypeControl: false,
+                mapTypeId: "satellite"
+              }}
+            >
+              {directions && (
+                <DirectionsRenderer
+                  directions={directions}
+                  options={{ suppressMarkers: true }}
+                />
+              )}
+              {waypointsMarkers.map((marker) => (
+                <Marker
+                  key={marker.key}
+                  position={marker.location}
+                  label={{
+                    text: marker.label.text,
+                    color: 'white',
+                    fontWeight: 'bold'
+                  }}
+                />
+              ))}
+            </GoogleMap>
+          </LoadScript>
         </div>
       </div>
       <div className="mt-[5%]">
@@ -165,28 +260,38 @@ const Mapa = () => {
             )}
           </div>
         ))}
-        <button
-          className="bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600"
-          onClick={handleOptimizeRoute}
-        >
-          Otimizar Rota
-        </button>
+        <div className='mb-8'>
+        {googleMapsUrl && (
+          <button
+            className="bg-blue-500 text-white p-3 rounded-lg shadow-lg hover:bg-blue-600 mt-4"
+            onClick={() => window.open(googleMapsUrl, '_blank')}
+          >
+            Abrir Rota no Maps
+          </button>
+        )}</div>
+        <div className='flex flex-column gap-8 justify-center'>
+          
+          <button
+            className="bg-green-500 text-white p-3 rounded-lg shadow-lg hover:bg-green-600"
+            onClick={handleAddAddress}
+          >
+            + Adicionar
+          </button>
+          <button
+            className="bg-blue-500 text-white p-3 rounded-lg shadow-lg hover:bg-blue-600"
+            onClick={handleOptimizeRoute}
+          >
+            Obter Rota
+          </button>
+        </div>
         {totalDistance && (
-          <div className="mt-2">
-            <p className="text-gray-700">Distância total: {totalDistance.toFixed(2)} km</p>
-            <a
-              href={googleMapsUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline"
-            >
-              Abrir no Google Maps
-            </a>
+          <div className="mt-2 text-black font-bold">
+            Distância total: {totalDistance.toFixed(2)} km
           </div>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default Mapa;
